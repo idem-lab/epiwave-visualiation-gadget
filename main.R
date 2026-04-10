b656dcba17d0683612dce8ac72de3729e00a1617
su <didtndus1187@gmail.com>
2026-04-10 15:56:43 +0800

    #9 Add a sample prior predictive check

--- main.R
+++ main.R
@@ -9,5 +9,10 @@ library(bayesplot)
 # Populate with real data and store return values in a variable
 fit <- fit_model(iris$Petal.Length,iris$Sepal.Length)
 
+# Prior Predictive Checks
+lines_df <- priors(iris$Petal.Length)
+
 # Take the variable as an input and run the gadget
 epiwaveVisualisationGadget(fit)
+
+
