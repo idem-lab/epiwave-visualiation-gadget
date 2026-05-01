8187a7adc32f65d3ccdb1d58df7391a82a076581
su <didtndus1187@gmail.com>
2026-05-01 16:38:11 +0800

    Update to allow null inputs, add plot download/copy UI buttons and server rendering, refactor script to call external R functions

--- epiwaveVisualisationGadget.R
+++ epiwaveVisualisationGadget.R
@@ -1,11 +1,12 @@
 
-# A function that takes in a fit model and visualises it for evaluation 
 
-epiwaveVisualisationGadget <- function(fit_model, info) {
+epiwaveVisualisationGadget <- function(fit_model=NULL, 
+                                       info=NULL, 
+                                       prior_results=NULL) {
 
 # Define UI for application
 ui <- fluidPage(
-      tags$head(includeCSS("www/styles.css"),includeScript("www/script.js")), # external css
+      tags$head(includeCSS("www/styles.css"), includeScript("www/script.js")), # external css
       
       navbarPage(
       id="main_nav",
@@ -21,7 +22,11 @@ ui <- fluidPage(
                    sliderInput("num", label="Number Input:", min=1, value=20, max=40)
                  ),
                  mainPanel(
-                   plotOutput("plot1")
+                   plotOutput("plotPriorParam"),
+                   downloadButton("downloadPriorParam", "Download Plot"),
+                   downloadButton("downloadPriorParamCode", "Download Code"),
+                   actionButton("copyPriorCode", "Copy Code"),
+                   actionButton("go","Screenshot")
                  )
               )
       ),
@@ -32,9 +37,11 @@ ui <- fluidPage(
                      sliderInput("num", label="Number Input:", min=1, value=20, max=40)
                     ),
                    mainPanel(
-                     plotOutput("plot2"),
-                     downloadButton("downloadPlot", "Download Plot"),
-                     actionButton("go","Screenshot")
+                     plotOutput("plotConv"),
+                     downloadButton("downloadConv", "Download Plot"),
+                     downloadButton("downloadConvCode", "Download Code"),
+                     actionButton("go","Screenshot"),
+                     actionButton("copyConvCode", "Copy Code")
                    )
                 )
             ),
@@ -45,8 +52,10 @@ ui <- fluidPage(
                      sliderInput("num", label="Number Input:", min=1, value=20, max=40)
                    ),
                    mainPanel(
-                     plotOutput("plot3"),
-                     downloadButton("downloadPlot2", "Download Plot"),
+                     plotOutput("plotPost"),
+                     downloadButton("downloadPost", "Download Plot"),
+                     downloadButton("downloadPostCode", "Download Code"),
+                     actionButton("copyPostCode", "Copy Code"),
                      actionButton("go","Screenshot")
                    )
                 )
@@ -55,89 +64,66 @@ ui <- fluidPage(
       
 )
 
-draw <- aperm(as.array(fit_model$fit),c(1,3,2))
-
 # Define server logic 
 server <- function(input, output, session) {
   
-    # prior predictive check 
-    # output$plot1 <- renderPlot({
-    #   ggplot(prior_results$sim_data, aes(x = x, y = y_rep, group = sim)) +
-    #     geom_line(alpha = 0.1, color = "red") +
-    #     geom_hline(yintercept = range(y), linetype = "dashed", color = "blue") +
-    #     theme_minimal() +
-    #     
-    #     labs(
-    #       title = "Prior Predictive Samples",
-    #       subtitle = "Simulated y values generated from prior parameter draws"
-    #     )
-    # })
-    
-    # convergence plot
-  output$plot2 <- renderPlot({
-    bayesplot::mcmc_trace(fit_model$fit)
-  })
+  #
+  # Prior Predictive Check plot
+  #
   
-  output$downloadPlot <- downloadHandler(
-    filename = function() {
-      paste0("convergence-plot-", Sys.Date(), ".png")
-    },
-    contentType = "image/png",
-    content = function(file) {
-      png(file, width = 1200, height = 800, res = 150)
-      
-      p <- bayesplot::mcmc_trace(fit_model$fit)
-      print(p)
-      
-      dev.off()
-    }
+  prior_param_plot <- reactive({
+    plot_prior_params(prior_results)
+  })
+  output$plotPriorParam <- renderPlot({
+    prior_param_plot()
+  })
+  output$downloadPriorParam <- download_plot_img(
+    p=prior_param_plot, fname="prior-param-plot"
   )
+  output$downloadPriorParamCode <- download_plot_code("plot_prior_check.R")
+  
+  #
+  # Convergence plot
+  #
+  
+  convergence_plot <- reactive({
+    create_convergence_plot(fit_model$fit)
+  })
+  output$plotConv <- renderPlot({
+    convergence_plot()
+  })
+  output$downloadConv <- download_plot_img(
+    p=convergence_plot, fname="convergence-plot"
+  )
+  output$downloadConvCode <- download_plot_code("plot_convergence.R")
+  
+  observeEvent(input$copyConvCode,{
+    copy_plot_code("plot_convergence.R")
+  })
+  # Allow user to screenshot the current page of the App
+  observeEvent(input$go,{screenshot()})
+  
+  
     
-    observeEvent(input$go,{
-      screenshot()
-    })
-    
-    # Posterior
-    output$plot3 <- renderPlot({
-        bayesplot::mcmc_areas(draw, pars=info$pars)
-        # scale_y_discrete(
-        #   labels=fit$lab
-        # ) +
-        # labs(
-        #   title="Posterior Distribution of model parameters",
-        #   subtitle="Bayesian Regression Model"
-        # ) +
-        # theme_gray() +
-        # theme(plot.title=element_text(hjust=0.5), plot.subtitle=element_text(hjust=0.5),
-        #       panel.border=element_rect(linetype="dashed"))
-    })
+  #
+  # Posterior plot
+  #
+  
+  posterior_plot <- reactive({
+    create_posterior_plot(fit_model, pars=info$pars)
+  })
+  output$plotPost <- renderPlot({
+    posterior_plot()
+  })
+  output$downloadPost <- download_plot_img(
+    posterior_plot, "posterior-plot"
+  )
+  output$downloadPostCode <- download_plot_code("plot_posterior_check.R")
+  observeEvent(input$go,{screenshot()})
 
-    
-    output$downloadPlot2 <- downloadHandler(
-      filename = function() {
-        paste0("posterior-plot-", Sys.Date(), ".png")
-      },
-      contentType = "image/png",
-      content = function(file) {
-        png(file, width = 1200, height = 800, res = 150)
-        
-        d <- bayesplot::mcmc_areas(draw, pars=info$pars)
-        print(d)
-        
-        dev.off()
-      }
-    )
-    
-  }
+  
+}
 
 # Run the application 
   shinyApp(ui = ui, server = server)
 }
-
-
-
-# server <- function(input, output) {
-#   # Create plot as a reactive to avoid repeating code
-#   plotInput <- reactive({
-#     ggplot(mtcars, aes(wt, mpg)) + geom_point()
-#   })
