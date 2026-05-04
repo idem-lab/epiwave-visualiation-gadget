912db79b6ea55864d45e590cadc176e28c23b1a5
su <didtndus1187@gmail.com>
2026-05-04 15:43:28 +0800

    #12 Fixed copy_plot_code.R

--- R/copy_plot_code.R
+++ R/copy_plot_code.R
@@ -2,7 +2,7 @@
 # A function that copies the code of the plot to clipboard via JS
 #
 
-copy_plot_code <- function(file) {
+copy_plot_code <- function(file, session) {
   code <- paste(readLines(file),collapse="\n")
   session$sendCustomMessage("copy-code",code)
 }
