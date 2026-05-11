1c179fc736c847f7b20057109105172248343652
su <didtndus1187@gmail.com>
2026-05-11 16:05:11 +0800

    Refactor for parameterisation of replacement strings

--- R/plot_convergence.R
+++ R/plot_convergence.R
@@ -1,10 +1,33 @@
-#
-# A function to create convergence plot 
-#
-
-create_convergence_plot <- function(model, pars=NULL) {
-  if (is.null(pars) || length(pars) == 0) {
-    pars <- colnames(model[[1]]) # Use all parameters when not given any input
+#' Title
+#'
+#' @param model 
+#' @param pars 
+#'
+#' @returns
+#' @export
+#'
+#' @examples
+#' 
+create_convergence_plot <- function(model, pars=NULL, param_map=NULL) {
+  
+  if (!is.null(model$fit)) {
+    model <- model$fit
   }
-  bayesplot::mcmc_trace(model, pars=pars)
+  
+  if (is.null(pars) || length(pars) == 0) {
+    pars <- colnames(model[[1]])
+  }
+  
+  if (!is.null(param_map)) {
+    labels <- setNames(param_map$display_name, param_map$raw_name)
+    pars <- ifelse(pars %in% names(labels), labels[pars], pars)
+    model <- setNames(lapply(model, function(chain) {
+      colnames(chain) <- labels[colnames(chain)]
+      chain
+    }), names(model))
+  }
+  bayesplot::mcmc_trace(model, pars=pars) # __PARS__
 }
+
+# RUN:
+# create_convergence_plot(model, pars) # __CALL__
