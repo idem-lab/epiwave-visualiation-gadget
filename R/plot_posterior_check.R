6b9cdd16cbd78006cf7cdf4e3213315a81708be2
su <didtndus1187@gmail.com>
2026-05-11 16:04:13 +0800

    #19 Modify to use `mcmc_dens_overlay` for separate plotting, Refactor for parameterisation of replacement strings

--- R/plot_posterior_check.R
+++ R/plot_posterior_check.R
@@ -1,13 +1,29 @@
-#
-# A function to create posterior plot 
-#
-
-create_posterior_plot <- function(model, pars=NULL) {
-  draw <- aperm(as.array(model$fit),c(1,3,2))
+#' Title
+#'
+#' @param model 
+#' @param pars 
+#'
+#' @returns
+#' @export
+#'
+#' @examples
+#' 
+create_posterior_plot <- function(model, pars=NULL, param_map=NULL) {
+  draw <- aperm(as.array(model$fit), c(1,3,2))
   
   if (is.null(pars) || length(pars) == 0) {
     pars <- dimnames(draw)[[3]]
   }
-  bayesplot::mcmc_areas(draw, pars)
+  
+  if (!is.null(param_map)) {
+    labels <- setNames(param_map$display_name, param_map$raw_name)
+    new_names <- labels[pars]
+    dimnames(draw)[[3]][dimnames(draw)[[3]] %in% pars] <- new_names
+    pars <- new_names
+  }
+  
+  bayesplot::mcmc_dens_overlay(draw, pars) # __PARS__
 }
 
+# RUN:
+# create_posterior_plot(model, pars) # __CALL__
